0acdc1ce45b16fd014ae15a9dab3012cb008d131
hossein1376 <hosseinyazdani1376@gmail.com>
2023-03-23 14:47:00 +0330

    added many functionalities to the front-end

--- src/pages/EventType.tsx
+++ src/pages/EventType.tsx
@@ -1,9 +1,10 @@
 import styles from "@/styles/Home.module.css";
-import { Divider, Image } from "antd";
+import { Divider, Image, Space } from "antd";
 
 interface Props {
   title: string;
   data: Events[] | undefined;
+  yearRange: [number, number];
 }
 
 type Events = {
@@ -36,34 +37,40 @@ type Page = {
   extract: string;
 };
 
-export default function EventType({ title, data }: Props): JSX.Element {
+export default function EventType({
+  title,
+  data,
+  yearRange,
+}: Props): JSX.Element {
   const events = data?.map((event) => {
-    return (
-      <>
-        <li className={styles.boxElement} key={event.year}>
-          <div>
-            <h3>{event.year}</h3>
-            <p>{event.text}</p>
-          </div>
-          {event.pages[0]?.thumbnail?.source &&
-            event.pages[0].thumbnail.width && (
-              <Image
-                width={event.pages[0].thumbnail.width}
-                src={event.pages[0].thumbnail.source}
-                alt={event.pages[0].title}
-              />
-            )}
-        </li>
-        <Divider />
-      </>
-    );
+    if (event.year >= yearRange[0] && event.year <= yearRange[1]) {
+      return (
+        <>
+          <li className={styles.boxElement} key={event.year}>
+            <div>
+              {event.year !== 0 && <h3>{event.year}</h3>}
+              <p>{event.text}</p>
+            </div>
+            {event.pages[0]?.thumbnail?.source &&
+              event.pages[0].thumbnail.width && (
+                <Image
+                  width={event.pages[0].thumbnail.width}
+                  src={event.pages[0].thumbnail.source}
+                  alt={event.pages[0].title}
+                />
+              )}
+          </li>
+          <Divider />
+        </>
+      );
+    }
   });
   return (
-    <div className={styles.box}>
+    <Space direction="vertical" size="large" className={styles.box}>
       <h2 className={styles.boxHeader}>{title}</h2>
       <div className={styles.boxBody}>
         <ul>{events}</ul>
       </div>
-    </div>
+    </Space>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,16 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import EventType from "./EventType";
 import { useEffect, useState } from "react";
-import { Skeleton } from "antd";
+import {
+  Checkbox,
+  Col,
+  Divider,
+  InputNumber,
+  Row,
+  Skeleton,
+  Slider,
+} from "antd";
+import type { CheckboxValueType } from "antd/es/checkbox/Group";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -50,6 +59,15 @@ export default function Home() {
   const [data, setData] = useState<Data>();
   const [loading, setLoading] = useState(true);
 
+  const [selected, setSelected] = useState(true);
+  const [events, setEvents] = useState(true);
+  const [holidays, setHolidays] = useState(false);
+  const [births, setBirths] = useState(false);
+  const [deaths, setDeaths] = useState(false);
+
+  const [yearRange, setYearRange] = useState<[number, number]>([0, 2023]);
+
+  const date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
   useEffect(() => {
     const url = "https://docker.iran.liara.run/events";
     fetch(url)
@@ -59,6 +77,30 @@ export default function Home() {
         setLoading(false);
       });
   }, []);
+
+  const groupChangeHandler = (checkedValues: CheckboxValueType[]) => {
+    setSelected(checkedValues.includes("Selected"));
+    setEvents(checkedValues.includes("Events"));
+    setHolidays(checkedValues.includes("Holidays"));
+    setBirths(checkedValues.includes("Births"));
+    setDeaths(checkedValues.includes("Deaths"));
+  };
+
+  const sliderChangeHandler = (values: [number, number]) => {
+    setYearRange(values);
+  };
+
+  const startYearRangeChangeHandler = (value: number | null) => {
+    if (value !== null) {
+      setYearRange([value, yearRange[1]]);
+    }
+  };
+
+  const endYearRangeChangeHandler = (value: number | null) => {
+    if (value !== null) {
+      setYearRange([yearRange[0], value]);
+    }
+  };
 
   return (
     <>
@@ -72,30 +114,120 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>On This Day:</h1>
         {loading && (
           <div className={styles.boxBody}>
-            <Skeleton paragraph={{ rows: 7 }} />
+            <Skeleton paragraph={{ rows: 8 }} />
             <br />
             <br />
-            <Skeleton paragraph={{ rows: 7 }} />
+            <Skeleton paragraph={{ rows: 8 }} />
           </div>
         )}
 
         {!loading && (
           <>
-            <EventType
-              title={"Selected Events"}
-              data={data?.selected}
-            ></EventType>
+            <div className={styles.head}>
+              <h1 className={styles.heading}>On This Day:</h1>
+              <p className={styles.subheading}>{date}</p>
 
-            <EventType title={"Events"} data={data?.events}></EventType>
+              <Checkbox.Group
+                defaultValue={["Selected", "Events"]}
+                onChange={groupChangeHandler}
+              >
+                <Row>
+                  <Col>
+                    <Checkbox value="Selected" className={styles.checkboxLabel}>
+                      Selected Events
+                    </Checkbox>
+                  </Col>
+                  <Col>
+                    <Checkbox value="Events" className={styles.checkboxLabel}>
+                      Events
+                    </Checkbox>
+                  </Col>
+                  <Col>
+                    <Checkbox value="Holidays" className={styles.checkboxLabel}>
+                      Holidays
+                    </Checkbox>
+                  </Col>
+                  <Col>
+                    <Checkbox value="Births" className={styles.checkboxLabel}>
+                      Births
+                    </Checkbox>
+                  </Col>
+                  <Col>
+                    <Checkbox value="Deaths" className={styles.checkboxLabel}>
+                      Deaths
+                    </Checkbox>
+                  </Col>
+                </Row>
+              </Checkbox.Group>
 
-            <EventType title={"Holidays"} data={data?.holidays}></EventType>
+              <Divider />
+              <p className={styles.yearRange}>Year Range:</p>
 
-            <EventType title={"Births"} data={data?.births}></EventType>
+              <Row className={styles.sliderBox}>
+                <InputNumber
+                  min={0}
+                  max={2023}
+                  value={yearRange[0]}
+                  onChange={(value) => startYearRangeChangeHandler(value)}
+                />
+                <Col style={{ width: "100%" }}>
+                  <Slider
+                    range={{ draggableTrack: true }}
+                    min={0}
+                    max={2023}
+                    defaultValue={[0, 2023]}
+                    onChange={sliderChangeHandler}
+                  />
+                </Col>
+                <InputNumber
+                  min={0}
+                  max={2023}
+                  value={yearRange[1]}
+                  onChange={(value) => endYearRangeChangeHandler(value)}
+                />
+              </Row>
+            </div>
+            {selected && (
+              <EventType
+                title={"Selected Events"}
+                data={data?.selected}
+                yearRange={yearRange}
+              ></EventType>
+            )}
 
-            <EventType title={"Deaths"} data={data?.deaths}></EventType>
+            {events && (
+              <EventType
+                title={"Events"}
+                data={data?.events}
+                yearRange={yearRange}
+              ></EventType>
+            )}
+
+            {holidays && (
+              <EventType
+                title={"Holidays"}
+                data={data?.holidays}
+                yearRange={yearRange}
+              ></EventType>
+            )}
+
+            {births && (
+              <EventType
+                title={"Births"}
+                data={data?.births}
+                yearRange={yearRange}
+              ></EventType>
+            )}
+
+            {deaths && (
+              <EventType
+                title={"Deaths"}
+                data={data?.deaths}
+                yearRange={yearRange}
+              ></EventType>
+            )}
           </>
         )}
       </main>
