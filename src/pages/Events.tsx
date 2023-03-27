db6e7651df3cfbd1928b31de54eea46b47eb469e
hossein1376 <hosseinyazdani1376@gmail.com>
2023-03-27 23:46:41 +0330

    dynamic

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,7 +1,8 @@
 import styles from "@/styles/Home.module.css";
-import { Space } from "antd";
-import type { Events } from "@/Types";
+import {Space} from "antd";
+import type {Events} from "@/Types";
 import EventDetails from "./EventDetails";
+import useWindowDimensions from "@/hooks/getWindowDimensions";
 
 interface Props {
   title: string;
@@ -9,22 +10,27 @@ interface Props {
   yearRange: [number, number];
 }
 
-export default function Events({ title, data, yearRange }: Props): JSX.Element {
+export default function Events({title, data, yearRange}: Props): JSX.Element {
+  const {height, width} = useWindowDimensions();
+  const size = width > 720 ? "large" : "small";
+
   const events = data?.map((event) => {
     if (event.year >= yearRange[0] && event.year <= yearRange[1]) {
       return (
         <ul key={event.pages[0].pageid}>
-          <EventDetails event={event} />
+          <EventDetails event={event}/>
         </ul>
       );
     }
   });
   return (
-    <Space direction="vertical" size="large" className={styles.box}>
-      <h2 className={styles.boxHeader}>{title}</h2>
-      <div className={styles.boxBody}>
-        <ul>{events}</ul>
-      </div>
-    </Space>
+    <>
+      <Space direction="vertical" size={size} className={styles.box}>
+        <h2 className={styles.boxHeader}>{title}</h2>
+        <div className={styles.boxBody}>
+          <ul>{events}</ul>
+        </div>
+      </Space>
+    </>
   );
 }
